552776a067604bef4cf6d143f932c1506ad5066f
Christopher Voit <development@christophervoit.de>
2021-07-30 22:07:51 +0200

    Tailwind fixed for real?C

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className='font-bold text-5xl text-red-700'>
+        Coming soon.
+      </div>
+    </div>
+  )
+}
+
+export default App
