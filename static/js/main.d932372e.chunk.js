af01b2ffd4c92b39e32829a38b00cb9162192fa6
Christopher Voit <development@christophervoit.de>
2021-07-30 22:10:09 +0200

    Updates

---
+++ static/js/main.d932372e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n.n(c),r=n(3),s=n.n(r),i=(n(8),n(0));var l=function(){return Object(i.jsx)("div",{className:"w-full h-screen flex justify-center items-center",children:Object(i.jsx)("div",{className:"font-bold text-5xl text-red-700",children:"Coming soon."})})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(l,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.d932372e.chunk.js.map
